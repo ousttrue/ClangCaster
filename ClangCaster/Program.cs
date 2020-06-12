d1971cecfd8da4db3dec2510de8ff92d8e1c1f0e
ousttrue <ousttrue@gmail.com>
2020-06-12 16:13:50 +0900

    init

---
+++ ClangCaster/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace ClangCaster
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var index = libclang.index.clang_createIndex(0, 1);
+            try
+            {
+                var source = Encoding.UTF8.GetBytes(args[0]);
+                var unsaved = new libclang.CXUnsavedFile
+                {
+
+                };
+                IntPtr cmd = default;
+                var tu = libclang.index.clang_parseTranslationUnit(index, ref source[0], ref cmd, 0, out unsaved, 0, 0);
+                Console.WriteLine(tu);
+            }
+            finally
+            {
+                libclang.index.clang_disposeIndex(index);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# ClangCaster
+
+* parse c++ header
+* generate cs source for DllImport
+
+## first version
+
+Generated https://github.com/ousttrue/regenerator .
