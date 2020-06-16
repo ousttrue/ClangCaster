61c6749b87858a48aff651d81ced2fec7b6440b4
ousttrue <ousttrue@gmail.com>
2020-06-16 14:51:48 +0900

    return type

--- ClangCaster/Exporter.cs
+++ ClangCaster/Exporter.cs
@@ -203,8 +203,32 @@ namespace {{ ns }}
             return false;
         }
 
+        /// <summary>
+        /// ClangCaster.Types.BaseType から CSharp の型を表す文字列と属性(struct用)を返す
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
         static (string, string) ToCSType(BaseType type)
         {
+            if (type is PrimitiveType primitiveType)
+            {
+                switch (primitiveType)
+                {
+                    case Int8Type int8Type: return ("sbyte", null);
+                    case Int16Type int16Type: return ("short", null);
+                    case Int32Type int32Type: return ("int", null);
+                    case Int64Type int64Type: return ("long", null);
+                    case UInt8Type uint8Type: return ("byte", null);
+                    case UInt16Type uint16Type: return ("ushort", null);
+                    case UInt32Type uint32Type: return ("uint", null);
+                    case UInt64Type uint64Type: return ("ulong", null);
+                    case Float32Type float32Type: return ("float", null);
+                    case Float64Type float64Type: return ("double", null);
+                    case VoidType voidType: return ("void", null);
+                }
+
+                throw new NotImplementedException();
+            }
             if (type is EnumType enumType)
             {
                 return (enumType.Name, null);
@@ -223,24 +247,6 @@ namespace {{ ns }}
             {
                 var elementType = ToCSType(arrayType.Element.Type).Item1;
                 return ($"{elementType}[]", $"[MarshalAs(UnmanagedType.ByValArray, SizeConst = {arrayType.Size})]");
-            }
-
-            switch (type)
-            {
-                case Int8Type int8Type: return ("sbyte", null);
-                case Int16Type int16Type: return ("short", null);
-                case Int32Type int32Type: return ("int", null);
-                case Int64Type int64Type: return ("long", null);
-                case UInt8Type uint8Type: return ("byte", null);
-                case UInt16Type uint16Type: return ("ushort", null);
-                case UInt32Type uint32Type: return ("uint", null);
-                case UInt64Type uint64Type: return ("ulong", null);
-                case VoidType voidType: return ("void", null);
-            }
-
-            if (GetPrimitive(type, out PrimitiveType primitive))
-            {
-                return ToCSType(primitive);
             }
 
             if (type is TypedefType typedefType)
@@ -277,9 +283,11 @@ namespace {{ ns }}
             {
                 var function = (FunctionType)src;
 
+                var csType = ToCSType(function.Result.Type).Item1;
+
                 return new
                 {
-                    Return = "void",
+                    Return = csType,
                     Name = function.Name,
                     Params = function.Params,
                 };
@@ -377,7 +385,7 @@ namespace {{ ns }}
                             w.Write(rendered);
                         }
 
-                        // BEGIN
+                        // END
                         w.Write(endTemplate.Render(DotLiquid.Hash.FromAnonymousObject(
                             new
                             {
